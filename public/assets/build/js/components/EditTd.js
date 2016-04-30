76740b0ef63df76cdf041eea187ec9e169c3a7de
Jesseyx <jyaoxiao@hotmail.com>
2016-04-30 23:24:41 +0800

    Continue do user jsx

--- public/assets/build/js/components/EditTd.js
+++ public/assets/build/js/components/EditTd.js
@@ -10,23 +10,60 @@ class EditTd extends Component {
         super(props);
 
         this.state = {
-            edit: false
+            editing: false,
+            value: props.value,
+            error: false,
+            submitting: false,
         }
+
+        this.handleEditClick = this.handleEditClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleEditClick() {
+        this.setState({
+            editing: true
+        }, () => {
+            this.refs.input.focus();
+        })
+    }
+
+    handleSubmit(e) {
+        var value = e.target.value.trim();
+        if (value === '' || value === this.props.value) {
+            return this.setState({
+                editing: false,
+            })
+        }
+        //
+        console.log('ajax submit!');
+        this.setState({
+            editing: false,
+            value,
+        })
     }
 
     render() {
-        const { className, value } = this.props;
+        const { value, props } = this.props;
 
-        if (this.state.edit) {
+        if (this.state.editing) {
             return (
-                <td className={ className }>
-                    <input value={ value } />
+                <td { ...props }>
+                    <input
+                        className="form-control"
+                        defaultValue={ this.state.value }
+                        onBlur={ this.handleSubmit }
+                        ref="input"
+                    />
                 </td>
             )
         }
         return (
-            <td className={ className }>
-                <p>{ this.props.value }</p>
+            <td
+                { ...props }
+                onClick={ this.handleEditClick }
+            >
+                <p>{ this.state.value }</p>
             </td>
         )
     }
