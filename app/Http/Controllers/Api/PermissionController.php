966b418d5bdf3c7d8774e41f676357dbf09628b6
Jesseyx <jyaoxiao@hotmail.com>
2016-05-12 14:45:19 +0800

    Do get permissions with their group

--- app/Http/Controllers/Api/PermissionController.php
+++ app/Http/Controllers/Api/PermissionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\PermissionGroup;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,6 +13,22 @@ class PermissionController extends Controller
     //
     public function getAllGroup()
     {
-        
+        // 渴求式加载
+        $groups = PermissionGroup::enable()->with(['permissions' => function ($query) {
+            return $query->enable();
+        }])->get();
+
+
+        foreach ($groups as $group) {
+            $group->children = $group->permissions;
+            $group->text = $group->name;
+            foreach ($group->permissions as $permission) {
+                $permission->text = $permission->name;
+            }
+
+            $group->id = $group->id;
+        }
+
+        return response()->json($groups);
     }
 }
--- app/PermissionGroup.php
+++ app/PermissionGroup.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class PermissionGroup extends Model
 {
     //
+    const STATUS_DISABLE = -1;
+    const STATUS_ENABLE = 1;
+
+    public function scopeEnable($query)
+    {
+        return $query->where('status', self::STATUS_ENABLE);
+    }
+
+    public function permissions()
+    {
+        return $this->hasMany('App\Permission', 'group_id');
+    }
 }
