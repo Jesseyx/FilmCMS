b071a9252cf20d79f41ada0aa82ea84e861eb41c
Jesseyx <jyaoxiao@hotmail.com>
2016-12-20 15:20:10 +0800

    Do not use view()->share()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,12 @@ class AppServiceProvider extends ServiceProvider
         //
         // 为视图组件附加登录的 user 对象
         // composer 会在视图每次渲染时都做绑定
-//        view()->composer('*', function ($view) {
-//            $view->with('_user', Auth::user());
-//        });
-        view()->share('_user', Auth::user());
+        view()->composer('*', function ($view) {
+           $view->with('_user', Auth::user());
+        });
+        // 用 share 共享视图数据会报错
+        // 参看下面的 issure: https://github.com/laravel/framework/issues/6130
+        // view()->share('_user', Auth::user());
     }
 
     /**
